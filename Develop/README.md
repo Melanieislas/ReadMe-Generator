46b4f3c409a427535549c2d2d29bf9a4a1b8dc77
Melanieislas <120453099+Melanieislas@users.noreply.github.com>
2023-03-09 22:19:19 -0800

    added table of contents and installation instruction to readme with terminal input

--- Develop/README.md
+++ Develop/README.md
@@ -1,3 +1,21 @@
-# j
+# Title
 
-# e
+## Description
+ 
+ - mot
+ - bui
+ - prob
+ - lear
+
+## Table of Contents
+
+ jr 
+
+ 
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [Credits](#credits)
+        - [License](#license)
+## Installation
+
+ ejre
--- Develop/index.js
+++ Develop/index.js
@@ -4,13 +4,14 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [
+const descriptiveQuestions = [
     'What was your motivation?', 
     'Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")', 
+    'What problem does it solve?',
     'What did you learn?'
 ];
 
-console.log(questions);
+//console.log(questions);
 
 inquirer
   .prompt([
@@ -21,13 +22,44 @@ inquirer
       },
     {
       type: 'input',
-      name: 'tite',
-      message: questions[0],
+      name: 'Descriptive0',
+      message: descriptiveQuestions[0],
+    },
+    {
+    type: 'input',
+      name: 'Descriptive1',
+      message: descriptiveQuestions[1],
+    },
+    {
+    type: 'input',
+      name: 'Descriptive2',
+      message: descriptiveQuestions[2],
+    },
+    {
+    type: 'input',
+        name: 'Descriptive3',
+        message: descriptiveQuestions[3],
+    },
+    {
+    type: 'input',
+        name: 'Contents',
+        message: "Enter instructions for your table of contents, to show the user how to navigate in your README",
+    },
+    {
+    type: 'input',
+        name: 'Installation',
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
     },
   ])
   .then((data) => {
     const writeToFile = `# ${data.title}\n` 
-        + `\n# ${data.tite}\n`;
+        + `\n## Description\n \n - ${data.Descriptive0}\n - ${data.Descriptive1}\n - ${data.Descriptive2}\n - ${data.Descriptive3}\n`
+        + `\n## Table of Contents\n\n ${data.Contents} \n\n 
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [Credits](#credits)
+        - [License](#license)\n`
+        + `\n## Installation\n\n ${data.Installation}\n`;
     
     console.log(data.title);
     const filename = `README.md`;
